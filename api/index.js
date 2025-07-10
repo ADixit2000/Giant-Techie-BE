36d8b3788f80d3dfdc0c001dfb367c800604f635
ADixit2000 <ankitdixit2701@gmail.com>
2025-07-11 02:11:36 +0530

    new changes

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
-// api/index.js
 const jsonServer = require("json-server");
-
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8000;
 
+// Do not use listen in serverless environment
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+module.exports = (req, res) => {
+  server(req, res);
+};
