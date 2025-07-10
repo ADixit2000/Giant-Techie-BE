6c92a4a07382690fe8c44c154a8250af12ef7bc7
ADixit2000 <ankitdixit2701@gmail.com>
2025-07-11 01:28:01 +0530

    code changes for deployment

---
+++ api/index.js
@@ -0,0 +1,11 @@
+// api/index.js
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+// Export the server as a Vercel-compatible function
+module.exports = server;
